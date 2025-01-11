32c2d9d7ac56f00d0fd11ec3179fc4119d11757f
Daniel Cui <solunian@gmail.com>
2025-01-11 10:50:06 -0800

    small fix edits

--- clap-detection/predict.py
+++ clap-detection/predict.py
@@ -8,7 +8,7 @@ from model import AudioClassifier
 
 def load_model(model_path):
     model = AudioClassifier()
-    model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu"))) # used to make model work on non-cuda gpu stuff... stuff
     model.eval()
     return model
 
--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-from datasets import load_dataset
+# from datasets import load_dataset
 
-ds = load_dataset("dvncoder/clap-detection")
+# ds = load_dataset("dvncoder/clap-detection")
+
+import torch
 
 def main():
     print("Hello from clapplay!")
+
+    trained_model = torch.load("./audio_classifier.pth")
+    
 
 
 if __name__ == "__main__":
