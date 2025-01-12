f1a1cccd4601c3cdba0fca18d5784a60b6024b0c
Aagrim Hoysal <aagrimhoysal@gmail.com>
2025-01-11 21:17:05 -0800

    added basic highpass filter

--- functionalDetector.py
+++ functionalDetector.py
@@ -7,7 +7,7 @@ import sys
 import math
 import time
 
-THRESHOLD_START = 0.1
+THRESHOLD_START = 0.04
 CLAP_MAX_HIGH_SECONDS = 0.05
 BACKGROUND_SECONDS = 10
 CHUNK_SECONDS = 0.025
@@ -49,26 +49,29 @@ def rootmeansquare(block):
     
     return math.sqrt(s / BLOCK_SIZE)
 
-# def highpass(block):
-#     BLOCK_SIZE = len(block) // 2
+def highpass(block):
+    BLOCK_SIZE = len(block) // 2
 
-#     highPassed = bytearray(len(block))
-#     rc = CHUNK_SECONDS / (len(block)/2)
-#     alpha = rc / (rc + CHUNK_SECONDS)
-#     print(alpha)
-#     highPassed[0] = block[0]
-#     highPassed[1] = block[1]
+    highPassed = bytearray(len(block))
+    # rc = CHUNK_SECONDS / (len(block)/2)
+    # alpha = rc / (rc + CHUNK_SECONDS)
+    # print(alpha)
+    highPassed[0] = block[0]
+    highPassed[1] = block[1]
 
-#     for i in range(1, BLOCK_SIZE):
-#         index = i << 1
-#         #print((struct.unpack('h', highPassed[index-2:index])[0], struct.unpack('h', block[index:index+2])[0], - struct.unpack('h', block[index-2:index])[0]))
-#         computed = alpha * (struct.unpack('h', highPassed[index-2:index])[0] + struct.unpack('h', block[index:index+2])[0] - struct.unpack('h', block[index-2:index])[0])
-#         highPassed[index], highPassed[index+1] = struct.pack('h', int(computed))
+    for i in range(1, BLOCK_SIZE):
+        index = i << 1
+        #print((struct.unpack('h', highPassed[index-2:index])[0], struct.unpack('h', block[index:index+2])[0], - struct.unpack('h', block[index-2:index])[0]))
+        #computed = (struct.unpack('h', highPassed[index-2:index])[0] + struct.unpack('h', block[index:index+2])[0] - struct.unpack('h', block[index-2:index])[0])
+        computed = struct.unpack('h', block[index:index+2])[0] - struct.unpack('h', block[index-2:index])[0]
+        highPassed[index], highPassed[index+1] = struct.pack('h', int(computed))
     
-#     return highPassed
+    return highPassed
 
+chunkBuffer = [0]*BACKGROUND_CHUNKS
+chunkIndex = -BACKGROUND_CHUNKS
 averageLow = 0
-averageSize = 0
+
 highChunks = 0
 
 p = pyaudio.PyAudio()
@@ -96,7 +99,6 @@ def expiry():
         if ratios[i] / minDistance > COMMAND_RATIO_THRESHOLD:
             ratioFlags += 1 << (len(ratios) - i - 1)
     
-    print(("{:%db}" % len(ratios)))
     print(("{:0%db}" % len(ratios)).format(ratioFlags))
     key = (1+len(ratios), ratioFlags)
 
@@ -124,9 +126,9 @@ def onClap(t):
 
 while not shouldQuit:
     block = stream.read(CHUNK)
-    # block = highpass(block)
-    #print(block)
+    block = highpass(block)
     rms = rootmeansquare(block)
+    #print(rms)
 
     if THRESHOLD_START < rms and not averageLow * 2 < rms:
         print(averageLow * 2, rms)
@@ -135,11 +137,17 @@ while not shouldQuit:
         highChunks += 1
     else:
         if 1 <= highChunks <= CLAP_MAX_HIGH_CHUNKS:
-            print("clap detected", rms, averageLow)
+            print("clap detected")
             onClap(time.time())
         else:
-            averageLow = (averageLow * averageSize + rms) / (averageSize + 1)
-            averageSize = min(averageSize + 1, BACKGROUND_CHUNKS)
+            chunkIndex = min(chunkIndex + 1, BACKGROUND_CHUNKS)
+            if chunkIndex < 0:
+                averageSize = chunkIndex + BACKGROUND_CHUNKS - 1
+                averageLow = (averageLow * averageSize + rms) / (averageSize + 1)
+            else:
+                chunkIndex %= BACKGROUND_CHUNKS
+                averageLow += (rms - chunkBuffer[chunkIndex]) / BACKGROUND_CHUNKS
+                chunkBuffer[chunkIndex] = rms
         highChunks = 0
     
     #shouldQuit = True
