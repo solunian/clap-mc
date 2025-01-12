feb25c2045200e54221763a97966ade0c2c7bbeb
Daniel Cui <solunian@gmail.com>
2025-01-12 02:40:08 -0800

    ui stuff

--- gui/src/routes/+page.svelte
+++ gui/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { invoke } from '@tauri-apps/api/core';
-  import { emit } from '@tauri-apps/api/event';
-  // import { onDestroy } from 'svelte';
+  // import { emit } from '@tauri-apps/api/event';
 
   let clap_on = $state(true);
   
@@ -9,7 +8,7 @@
     if (clap_on) {
       invoke("call_my_sidecar");
     } else {
-      invoke("kill", { name: "hi" });
+      invoke("kill");
     }
   });
 
@@ -18,9 +17,27 @@
   // }
 </script>
 
-this is a toggle for keeping the binary active 
-<input type="checkbox" bind:checked={clap_on}>
 
-{clap_on}
+<div>
+Clap to control your computer's playback!
+</div>
+
+<div>
+2 claps
+♫ - two claps - play/pause
+</div>
+
+<div>
+Selection: 3 claps
+♩♫ - one long clap, two short claps - go to next 
+♪♩♩- one short clap, two long claps - go to previous
+</div>
+
+<div>
+Volume: 4 claps
+♩♩♫ - two long claps, two short claps - volume up
+♩ ♪♩♪- long, short, long, short - volume down
+♩♫♪ - one long clap, three short claps - mute
+</div>
 
 <!-- <button onclick={click}>kill child</button> -->
